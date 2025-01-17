bc1f1836d0ce2d4af4b5c8424a73bbe501c00a0b
ai-ahsan <muhammad.ahsan@gaditek.com>
2025-01-17 19:26:23 +0500

    Integerated Tavily Search

--- tools.py
+++ tools.py
@@ -1,6 +1,8 @@
 from crewai_tools import JSONSearchTool
 from crewai.tools import tool
 from tavily import TavilyClient
+import os
+
 
 json_tool_faq = JSONSearchTool(json_path='JSONS/FAQ.json')
 json_tool_inventory = JSONSearchTool(json_path='JSONS/Inventory.json')
@@ -10,6 +12,6 @@ json_tool_inventory = JSONSearchTool(json_path='JSONS/Inventory.json')
 @tool("Tavily Search")
 def tavily_search(query: str) -> str:
     """Perform a search using Tavily API."""
-    tavily_client = TavilyClient(api_key="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")
-    context = tavily_client.get_search_context(query="What happened during the Burning Man floods?")
+    tavily_client = TavilyClient(api_key=os.environ.get('TAVILY_API_KEY'))
+    context = tavily_client.get_search_context(query)
     return context
